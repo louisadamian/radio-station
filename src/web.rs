a39760e8f1861b65b4fccb32079d40f1a93010d8
Louis Adamian <louis@adamian.net>
2025-04-02 16:10:46 -0400

    add functional actix websocket server

--- src/web.rs
+++ src/web.rs
@@ -1,31 +1,74 @@
 use actix_files::Files;
 use actix_web::{App, Error, HttpRequest, HttpResponse, HttpServer, rt, web};
 use actix_ws::AggregatedMessage;
+use bytestring::ByteString;
 use futures_util::StreamExt as _;
-async fn socket(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let (res, mut session, stream) = actix_ws::handle(&req, stream)?;
+use std::sync::Arc;
+use std::thread;
+use tokio::sync::{Mutex, watch};
+use tokio::time::{Duration, Instant, interval};
 
-    let mut stream = stream
-        .aggregate_continuations()
-        .max_continuation_size(2_usize.pow(20));
+async fn ws(req: HttpRequest, body: web::Payload) -> Result<HttpResponse, actix_web::Error> {
+    let (response, mut session, stream) = actix_ws::handle(&req, body)?;
+    let mut stream = stream.max_frame_size(128 * 1024).aggregate_continuations();
+    tracing::info!("Inserted session");
+    let alive = Arc::new(Mutex::new(Instant::now()));
+    let (weather_brief_sender, mut weather_brief_receiver) = watch::channel(String::new());
+    tokio::spawn(async move {
+        let messages = [
+            "24&deg;F light snow 🌨".to_string(),
+            "45&deg;F Sunny ☀".to_string(),
+            "80&deg;F light rain 🌦️".to_string(),
+        ];
+        let mut i = 0;
+        let mut wait = interval(Duration::from_secs(5));
+        loop {
+            wait.tick().await;
+            weather_brief_sender.send(messages[i].clone()).unwrap();
+            i = (i + 1) % messages.len();
+        }
+    });
+    let mut session2 = session.clone();
+    let alive2 = alive.clone();
+    actix_web::rt::spawn(async move {
+        let mut wait = actix_web::rt::time::interval(Duration::from_secs(5));
+        let mut w = weather_brief_receiver.clone();
+        loop {
+            wait.tick().await;
+            if w.has_changed().unwrap() {
+                let s = w.borrow_and_update().clone();
+                session2.text(ByteString::from(s)).await.unwrap();
+            }
 
-    rt::spawn(async move {
-        while let Some(msg) = stream.next().await {
-            match msg {
-                Ok(AggregatedMessage::Text(text)) => {
-                    session.text(text).await.unwrap();
-                }
-                Ok(AggregatedMessage::Binary(bin)) => {
-                    session.binary(bin).await.unwrap();
-                }
-                Ok(AggregatedMessage::Ping(msg)) => {
-                    session.pong(&msg).await.unwrap();
-                }
-                _ => {}
+            if session2.ping(b"").await.is_err() {
+                break;
+            }
+
+            if Instant::now().duration_since(*alive2.lock().await) > Duration::from_secs(10) {
+                let _ = session2.close(None).await;
+                break;
             }
         }
     });
-    Ok(res)
+
+    actix_web::rt::spawn(async move {
+        while let Some(Ok(msg)) = stream.recv().await {
+            match msg {
+                AggregatedMessage::Ping(bytes) => {
+                    if session.pong(&bytes).await.is_err() {
+                        return;
+                    }
+                }
+                AggregatedMessage::Pong(_) => {
+                    *alive.lock().await = Instant::now();
+                }
+                _ => (),
+            };
+        }
+        let _ = session.close(None).await;
+    });
+    tracing::info!("Spawned");
+    Ok(response)
 }
 
 pub async fn web() -> std::io::Result<()> {
@@ -33,7 +76,7 @@ pub async fn web() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(Files::new("/", "static").index_file("index.html"))
-            .route("/socket", web::get().to(socket))
+            .route("/ws", web::get().to(ws))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- static/js/weather.js
+++ static/js/weather.js
@@ -1,11 +1,37 @@
-const weatherTexts = document.getElementsByClassName("weather-report")
-weatherTexts[0].innerHTML = "Weather: 60&deg;F partly cloudy &#9925;";
+const weatherText = document.getElementsByClassName("weather-report")
+weatherText.innerHTML = "Weather: 60&deg;F partly cloudy &#9925;";
+/** @type {WebSocket | null} */
+let socket = null;
 
-function updateWeatherReport(){
-    rep = "Weather: "+"53&deg;F Coudy ☁️";
-    document.getElementsById("weather-report").innerHTML = rep;
+function updateWeatherReport(report){
+    document.getElementById("weather-report").innerHTML = "Weather: " + report;
 }
-function onImgSelect(){
-    changeGoesImage(document.getElementById("goes-select").value);
 
+function disconnect() {
+    if (socket) {
+        console.log('Disconnecting...')
+        socket.close()
+        socket = null
+    }
 }
+
+function connect(){
+    disconnect();
+    const { location } = window;
+    const proto = location.protocol.startsWith("https")? "wss" : "ws";
+    const wsUri = `${proto}://${location.host}/ws`;
+    socket = new WebSocket(wsUri);
+
+    socket.onopen= () => {
+        console.log('Connected');
+    }
+    socket.onmessage = ev => {
+        updateWeatherReport(ev.data)
+        console.log('Received: ' + ev.data, 'message')
+    }
+    socket.onclose = () => {
+        console.log('Disconnected')
+        socket = null
+    }
+}
+connect()
